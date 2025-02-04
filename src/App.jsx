cd6b0c5f6cb3f9b1bdebc3838051d50623793df0
Fabricio <fabricio.pimentel@starburstdata.com>
2025-02-04 15:39:36 -0500

    add apmplify_output

--- src/App.jsx
+++ src/App.jsx
@@ -47,6 +47,7 @@ import { Amplify } from "aws-amplify";
 import "@aws-amplify/ui-react/styles.css";
 import { generateClient } from "aws-amplify/data";
 import outputs from "../amplify_outputs.json";
+
 /**
  * @type {import('aws-amplify/data').Client<import('../amplify/data/resource').Schema>}
  */
